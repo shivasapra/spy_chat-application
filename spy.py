034a6002f64f1759b8fc28d6a08944e79128a6bb
shiva <ashishsapra7@gmail.com>
2017-06-30 01:32:54 -0700

    edge cases and scenarios handled

--- spy.py
+++ spy.py
@@ -42,7 +42,7 @@ def read_message():
 def spy_detail():
     temp = 0
     while temp == 0:
-        spy_name = raw_input("\nEnter Name\n----------------------")
+        spy_name = raw_input("\n****************\nEnter Name")
         try:
             if type(int(spy_name)) == int:
                 temp = 0
@@ -51,33 +51,30 @@ def spy_detail():
 
     salutation = ' '
     while salutation != 'mr.' and salutation != 'ms.':
-        salutation = raw_input("\nwhat should we call (mr.or ms.)\n----------------------")
+        salutation = raw_input("****************\nwhat should we call (mr.or ms.)")
     spy_salutation = salutation
     spy_name = spy_salutation + spy_name
 
     age = "not ok"
     while age == "not ok":
-        spy_age = raw_input("\nplease enter age\n----------------------")
+        spy_age = raw_input("****************\nplease enter age")
         try:
             if type(int(spy_age)) == int:
                 age = "ok"
         except:
-            print("*************************************************")
-            print("ENTER YOUR DETAILS CAREFULLY")
-            print("*************************************************")
+            print "\n!!!!!!!!!!!!!!!\nenter carefully\n!!!!!!!!!!!!!!!\n"
             age = "not ok"
 
     rating = "not ok"
     while rating == "not ok":
         spy_rating = raw_input(
-            "\nplease enter spy rating (x.y out of 10 or an integer out of 100 )\n----------------------")
+            "****************\nplease enter spy rating (out of 10)")
+        print "****************"
         try:
             if type(float(spy_rating)) == float:
                 rating = "ok"
         except:
-            print("*************************************************")
-            print("ENTER YOUR DETAILS CAREFULLY")
-            print("*************************************************")
+            print "\n!!!!!!!!!!!!!!!\nenter carefully\n!!!!!!!!!!!!!!!\n"
             rating = "not ok"
 
     spy_is_online = True
@@ -93,7 +90,9 @@ def spy_detail():
 
 def select_a_friend():
     if len(friends) > 0:
-        print "here are your friends"
+        print "\n*********************" \
+              "\nhere are your friends" \
+              "\n*********************"
         for spy in friends:
             print "*****************************************************"
             print "% s aged % d with rating % f is online" % (spy['name'], int(spy['age']), float(spy['rating']))
@@ -105,89 +104,112 @@ def select_a_friend():
         index = int(raw_input('choose option')) - 1
         return index
     else:
-        print("you have no friends")
+        print("\n!!!!!!!!!!!!!!!!!!!"
+              "\nyou have no friends"
+              "\n!!!!!!!!!!!!!!!!!!!")
 
 
 def add_friend():
     friends_detail = spy_detail()
-    friends.append(friends_detail)
+    if friends_detail['age'] > '18' and friends_detail['age'] < '60':
+        friends.append(friends_detail)
+    else:
+        print "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" \
+              "\nyou'r friend is not eligible for a spy in this age" \
+              "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     return len(friends)
 
 
 def add_status(current_status_message):
 
     if current_status_message != None:
-        print "\n your current status message is" + current_status_message + "\n----------------------"
-    else:
-        print "\n you don't have any status currently\n----------------------"
-    default = raw_input("\n \ndo you want to select from the older status (y/n)\n----------------------")
-    if default.upper() == 'N':
-        new_status_message = raw_input("enter status message\n----------------------")
-
-        if len(new_status_message) > 0:
-            updated_status_message = new_status_message
-            status_messages.append(updated_status_message)
-            return updated_status_message
-
-    elif default.upper() == 'Y':
-        item_position = 1
-        for message in status_messages:
-            print str(item_position) + " " + message + "\n----------------------"
-            item_position =item_position + 1
-        message_selection = int(raw_input("choose a status\n----------------------"))
-        if len(status_messages) >= message_selection:
-            updated_status_message = status_messages[message_selection-1]
-            return updated_status_message
+        print "\n*******************************\n your current status message is" \
+              + " " + current_status_message + "\n*******************************"
 
     else:
-        current_status_message=None
-        add_status(current_status_message)
+        print "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" \
+              "\n you don't have any status currently" \
+              "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+    temp = "not ok"
+    while temp == "not ok":
+        default = raw_input("\n***********************************************\n"
+                            "do you want to select from the older status (y/n)"
+                            "\n***********************************************")
+        if default.upper() == 'N':
+            temp = "ok"
+            new_status_message = raw_input("\n*********************\nenter status message")
+            print "*********************"
+            if len(new_status_message) > 0:
+                updated_status_message = new_status_message
+                status_messages.append(updated_status_message)
+                return updated_status_message
+
+        elif default.upper() == 'Y':
+            temp = "ok"
+            item_position = 1
+            for message in status_messages:
+                print "\n" + str(item_position) + " " + message
+                item_position =item_position + 1
+            message_selection = int(raw_input("\nchoose a status"))
+            if len(status_messages) >= message_selection:
+                updated_status_message = status_messages[message_selection-1]
+                return updated_status_message
+
+        else:
+            temp = "not ok"
 
 
 def start_chat(spy_name, spy_age, spy_rating,):
     current_status_message = None
     if spy_age > '18' and spy_age < '60':
-        print "*****************************************************"
-        print "\nwelcome " + spy_name + "\n age: " + spy_age + " \n rating: " + spy_rating
-        print "*****************************************************"
+        print "\n\n\n**************************"
+        print "welcome " + spy_name + "\n age: " + spy_age + " \n rating: " + spy_rating
+        print "**************************"
         show_menu = True
 
-        while show_menu:
+        while 1:
             menu_choice = int(raw_input("\n**********what do you want to do?********** \n "
                                         "1. update a status \n 2.Add a friend \n "
                                         "3. send secret message to a friend \n"
                                         "4. read a secret message\n"
-                                        "5.exit application\n----------------------"))
+                                        "5.exit application\n"
+                                        "**********************************************\n"))
             if menu_choice == 1:
-                print "\nyou have to update the status\n----------------------"
-                message = add_status(current_status_message)
-                print "*****************************************************"
-                print "\nyour status message is"
-                print message + "\n----------------------"
-                print "*****************************************************"
+                current_status_message = add_status(current_status_message)
+                print "\n******************************"
+                print "your status message is"
+                print current_status_message
+                print "**********************************"
             elif menu_choice == 2:
-                no_of_friends = str(add_friend())
-                print no_of_friends + " friend/'s added"
+                no_of_friends = (add_friend())
+                print "\n**********" + str(no_of_friends) + " friend/s added" + "**********"
             elif menu_choice == 3:
                 send_message()
             elif menu_choice == 4:
                 read_message()
+            elif menu_choice == 5:
+                exit()
             else:
-                show_menu = False
+                print "\n!!!!!!!!!!!!!!!\nenter carefully\n!!!!!!!!!!!!!!!"
     else:
-        print "\nyou are not eligible for a spy in this age\n----------------------"
+        print "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" \
+              "\nyou are not eligible for a spy in this age" \
+              "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
-print("\n welcome!!!!.... \n----------------------")
+print("***************\n welcome!!!!.... \n***************")
 
-proceed = " "
-while proceed.lower() != 'y' and proceed.lower() != 'n':
-    proceed = raw_input("want to proceed with default user (y/n)")
+proceed = "not ok"
+while proceed == "not ok":
+    proceed = raw_input("\nwant to proceed with default user (y/n)")
     if proceed.lower() == 'y':
+        proceed = "ok"
         start_chat(spy_salutation + spy_name, spy_age, spy_rating)
 
     elif proceed.lower() == 'n':
         details = spy_detail()
+        proceed = "ok"
         start_chat(details['name'], details['age'], details['rating'],)
-
     else:
-        print "enter valid option\n----------------------"
+        print "\n!!!!!!!!!!!!!!!\nENTER CAREFULLY\n!!!!!!!!!!!!!!!"
+        proceed = "not ok"
--- spy_details.py
+++ spy_details.py
@@ -3,4 +3,3 @@ spy_salutation = "Mr."
 spy_age = '20'
 spy_rating = '8.9'
 spy_is_online = True
-
